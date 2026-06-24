88a312dcd0470e4438bf4aa99592b32e1fe6f7bd
RykerWilder <fabiomasciarelliwd@gmail.com>
2026-06-24 18:07:38 +0200

    fix: remove dotenv

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import holehe, sherlock, abuseipdb, whois, dorks
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = FastAPI(title="CyberSec Dashboard API", version="1.0.0")
 
